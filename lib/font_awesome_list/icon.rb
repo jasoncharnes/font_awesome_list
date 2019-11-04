847f410f3c330ee9cefd6bbcf47bc89f1da86a28
Jason Charnes <jason@thecharnes.com>
2019-11-04 09:26:46 -0600

    Refactors all into a hash

--- lib/font_awesome_list/icon.rb
+++ lib/font_awesome_list/icon.rb
@@ -2,11 +2,16 @@ require "yaml"
 
 module FontAwesomeList
   class Icon
-    ICON_FILE = "#{__dir__}/icons.yml".freeze
-
     def self.all
-      icons = File.read(ICON_FILE)
-      YAML.load(icons).keys
+      icons.map { |k,v| { name: k, styles: v["styles"].map(&:to_sym) } }
+    end
+
+    def self.icons
+      YAML.load File.read("#{__dir__}/icons.yml")
+    end
+
+    def self.names
+      icons.keys
     end
   end
 end
