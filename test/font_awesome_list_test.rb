04a579f3a4ba5f215e07301f5c92d0d905a8e239
Jason Charnes <jason@thecharnes.com>
2019-11-04 09:30:03 -0600

    Updates tests

--- test/font_awesome_list_test.rb
+++ test/font_awesome_list_test.rb
@@ -2,22 +2,30 @@ require "test_helper"
 require "yaml"
 
 class FontAwesomeListTest < Minitest::Test
+  def setup
+    icon_file = File.read("#{__dir__}/../lib/font_awesome_list/icons.yml")
+    @icons = YAML.load(icon_file)
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::FontAwesomeList::VERSION
   end
 
   def test_that_it_has_all_the_icons
-    icon_file = File.read("#{__dir__}/../lib/font_awesome_list/icons.yml")
-    icons = YAML.load(icon_file)
-
-    assert_equal icons.count, FontAwesomeList::Icon.all.count
+    assert_equal @icons.count, FontAwesomeList::Icon.all.count
   end
 
-  def test_that_it_just_returns_the_keys
-    icon_file = File.read("#{__dir__}/../lib/font_awesome_list/icons.yml")
-    icons = YAML.load(icon_file).keys
+  def test_that_it_has_all_the_icons
+    assert_equal @icons.count, FontAwesomeList::Icon.all.count
+  end
 
+  def test_that_it_can_return_the_names_with_the_styles
     assert_kind_of Array, FontAwesomeList::Icon.all
-    assert_equal "500px", FontAwesomeList::Icon.all.first
+    assert_equal FontAwesomeList::Icon.all.first, {name: "500px", styles: [:brands]}
+  end
+
+  def test_that_it_can_return_the_names
+    assert_kind_of Array, FontAwesomeList::Icon.names
+    assert_equal "500px", FontAwesomeList::Icon.names.first
   end
 end
